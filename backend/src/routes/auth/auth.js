005988fb5bbbd40d74e2bde65357288a26f27523
Alexandre Schaffner <alexandre.schaffner@epitech.eu>
2021-08-14 22:49:19 +0200

    Google auth process refactored

--- backend/src/routes/auth/auth.js
+++ backend/src/routes/auth/auth.js
@@ -15,7 +15,7 @@ module.exports = async function (app, db, credentials, oauth2ClientMap) {
     res.send('Credentials are correct')
   })
 
-  app.get('/oauth2callback', function (req, res) {
+  app.get('/oauth2callback', async function (req, res) {
     const collection = db.collection('users')
     const code = req.query.code
     const oauth2Client = new OAuth2(
@@ -30,40 +30,36 @@ module.exports = async function (app, db, credentials, oauth2ClientMap) {
         res.send('Sorry, an error occured: ' + req.query.error)
       }
     } else {
-      oauth2Client.getToken(code, (err, token) => {
-        if (err) {
-          console.log('Error while trying to retrieve access token', err)
-        } else {
-          oauth2Client.setCredentials(token)
-          oauth2Client.getTokenInfo(oauth2Client.credentials.access_token).then((infos, err) => {
-            if (err) {
-              console.log(err)
-            } else {
-              collection.updateOne(
-                { _id: infos.email },
-                {
-                  $set: {
-                    google: {
-                      userId: infos.sub,
-                      tokens: {
-                        accessToken: oauth2Client.credentials.access_token,
-                        refreshToken: oauth2Client.credentials.refresh_token
-                      }
+      try {
+        oauth2Client.getToken(code, async (err, token) => {
+          if (err) {
+            throw new Error(err)
+          } else {
+            oauth2Client.setCredentials(token)
+            const infos = await oauth2Client.getTokenInfo(oauth2Client.credentials.access_token)
+            console.log(oauth2Client.credentials)
+            await collection.updateOne(
+              { _id: infos.email },
+              {
+                $set: {
+                  google: {
+                    userId: infos.sub,
+                    tokens: {
+                      accessToken: oauth2Client.credentials.access_token,
+                      refreshToken: oauth2Client.credentials.refresh_token
                     }
                   }
                 }
-              ).then((resolve, error) => {
-                if (error) {
-                  throw error
-                } else {
-                  oauth2ClientMap.set(infos.sub, oauth2Client)
-                }
-              })
-            }
-          })
-          res.redirect('/logged')
-        }
-      })
+              }
+            )
+            oauth2ClientMap.set(infos.sub, oauth2Client)
+            res.redirect('/logged')
+          }
+        })
+      } catch (err) {
+        res.send(err)
+        console.error(err)
+      }
     }
   })
 }
