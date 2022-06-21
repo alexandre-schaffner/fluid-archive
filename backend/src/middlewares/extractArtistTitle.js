3cefbc358f61a7388cf816b19b37e1714da766c9
Alexandre Schaffner <alexandre.schaffner@epitech.eu>
2022-06-21 18:38:41 +0200

    [UPDATE] extraction of artist and title

--- backend/src/middlewares/extractArtistTitle.js
+++ backend/src/middlewares/extractArtistTitle.js
@@ -1,5 +1,5 @@
 module.exports = (videoTitle) => {
-  const parasites = [' x ', ' feat', ' ft. ', ' | ', ' /', ' & ', ' (', ' [']
+  const parasites = [' x ', ' feat', ' ft. ', ' | ', ' /', /* ' & ', */ ' (', ' [']
   const splitedVideoTitle = videoTitle.split(' - ', 2)
   let artist = null
   let title = null
@@ -10,16 +10,11 @@ module.exports = (videoTitle) => {
     throw new Error('no artist or title found in the video title')
   }
   for (const token of parasites) {
-    artist.replace(token, '')
-    title.replace(token, '')
+    artist = artist.split(token, 1)[0]
+    title = title.split(token, 1)[0]
   }
   return ({
     artist,
     title
   })
 }
-// try {
-//   console.log(extractArtistTitle(videoTitle))
-// } catch (err) {
-//   console.error(err.message)
-// }
