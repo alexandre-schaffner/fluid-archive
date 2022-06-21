13c2695bcf3bc4890410caded49aaa00e614c10f
Alexandre Schaffner <alexandre.schaffner@epitech.eu>
2022-06-21 13:28:24 +0200

    [ADD] getTrackID middleware

---
+++ backend/src/middlewares/getTrackID.js
@@ -0,0 +1,16 @@
+const axios = require('axios').default
+
+module.exports = async ({ artist, title }) => {
+  const response = await axios.get('https://api.deezer.com/search/track', {
+    params: {
+      q: title
+    }
+  })
+  for (let i = 0; i < response.total; i++) {
+    if (
+      response.data[i].artist.name === artist &&
+      response.data[i].title === title
+    ) return response.data[i].id
+  }
+  throw new Error('track not found')
+}
